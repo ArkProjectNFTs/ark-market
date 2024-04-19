5fc6667ab130be2ed12ef7115661dab6e2bec5f2
kwiss <mr.dumont.christophe@gmail.com>
2024-04-19 12:37:40 +0200

    chore(app): update app organisation

--- tooling/tailwind/base.ts
+++ tooling/tailwind/base.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["src/**/*.{ts,tsx}"],
+  content: ["**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
