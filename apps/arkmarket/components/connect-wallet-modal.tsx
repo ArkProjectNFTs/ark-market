f6350177dcda9f02475faeb6c730e7c7dc4c7929
Yohan Tancrez <yohantancrez@gmail.com>
2024-04-22 03:49:17 +0200

    added loading state in connect wallet options

--- apps/arkmarket/components/connect-wallet-modal.tsx
+++ apps/arkmarket/components/connect-wallet-modal.tsx
@@ -1,4 +1,6 @@
+import type { Connector } from "@starknet-react/core";
 import type { PropsWithChildren } from "react";
+import { useState } from "react";
 import { useConnect } from "@starknet-react/core";
 
 import { Button } from "@ark-market/ui/components/button";
@@ -12,7 +14,21 @@ import {
 } from "@ark-market/ui/components/dialog";
 
 export default function ConnectWalletModal({ children }: PropsWithChildren) {
-  const { connectors, connect } = useConnect();
+  const { connectors, connectAsync } = useConnect();
+
+  const [pendingConnectorId, setPendingConnectorId] = useState<
+    string | undefined
+  >(undefined);
+
+  async function connect(connector: Connector) {
+    setPendingConnectorId(connector.id);
+    try {
+      await connectAsync({ connector });
+    } catch (error) {
+      console.error(error);
+    }
+    setPendingConnectorId(undefined);
+  }
 
   return (
     <Dialog>
@@ -26,9 +42,11 @@ export default function ConnectWalletModal({ children }: PropsWithChildren) {
         </DialogHeader>
         <div className="mt-4 flex flex-col gap-2">
           {connectors.map((connector) => {
+            const isConnecting = pendingConnectorId === connector.id;
+
             return (
-              <Button key={connector.id} onClick={() => connect({ connector })}>
-                {connector.name}
+              <Button key={connector.id} onClick={() => connect(connector)}>
+                {isConnecting ? "Loading..." : connector.name}
               </Button>
             );
           })}
