1e9f600951dcdfba713ac040ba79afa970143955
kwiss <mr.dumont.christophe@gmail.com>
2024-06-17 19:45:21 +0200

    fix: package-lock

---
+++ apps/arkmarket/config/homepage.ts
@@ -0,0 +1,116 @@
+export const collections = [
+  {
+    name: "Blobert",
+    address:
+      "0x00539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1",
+    image: "/collections/c6fc5552-1051-4f68-87c9-fcd6ddc1f026.jpeg",
+  },
+  {
+    name: "Starknet.id",
+    address:
+      "0x05dbdedc203e92749e2e746e2d40a768d966bd243df04a6b712e222bc040a9af",
+    image: "/collections/59685ea9-aeae-4784-82a1-5aa9364cea9b.png",
+  },
+  {
+    name: "Alter Nova",
+    address:
+      "0x0169e971d146ccf8f5e88f2b12e2e6099663fb56e42573479f2aee93309982f8",
+    image: "/collections/2024030811331141bb5ac7adfa420ab24b813ece99b01a.gif",
+  },
+  {
+    name: "Starknet Quest",
+    address:
+      "0x076503062d78f4481be03c9145022d6a4a71ec0719aa07756f79a2384dc7ef16",
+    image: "/collections/fff43877-5668-4392-bced-f95f6faa8dd8.png",
+  },
+  {
+    name: "Argent: Xplorer",
+    address:
+      "0x01b22f7a9d18754c994ae0ee9adb4628d414232e3ebd748c386ac286f86c3066",
+    image: "/collections/eee0d689-bf44-4a77-bdbe-eeeceb6d4b64.png",
+  },
+  {
+    name: "The Crown of Stark",
+    address:
+      "0x041302e7d6c1329ceb18df33107ff285e3a46329f281aba2b0eb957240649760",
+    image: "/collections/6fe90ed7-7b32-4a0d-a42b-bc316873112c.jpg",
+  },
+  {
+    name: "Dragark Element NFT",
+    address:
+      "0x0537fa10cefbb8ff7e61e86e950746809f95faa7398f250f0063069b29bb7933",
+    image: "/collections/1.png",
+  },
+  {
+    name: "briq Sets",
+    address:
+      "0x01435498bf393da86b4733b9264a86b58a42b31f8d8b8ba309593e5c17847672",
+    image: "/collections/b0c8387c-08cf-4b7d-8c5a-4d743bfcc727.png",
+  },
+  {
+    name: "Spok",
+    address:
+      "0x07606cac9053e9b8b573a4b0a0ce608880f64869e24b8a605210d7a85bb6e5f1",
+    image: "/collections/522ae053-3dfa-4305-8bbe-2381d581cb9b.png",
+  },
+  {
+    name: "Starkurabu",
+    address:
+      "0x03ab1124ef9ec3a2f2b1d9838f9066f9a894483d40b33390dda8d85c01a315a3",
+    image: "/collections/7d9903ba-ec50-4474-b2b1-7eb4fbc264bd.png",
+  },
+  {
+    name: "StarkRock",
+    address:
+      "0x012f8e318fe04a1fe8bffe005ea4bbd19cb77a656b4f42682aab8a0ed20702f0",
+    image: "/collections/f90e79fe-2477-461c-a4df-4f62c4134a33.png",
+  },
+  {
+    name: "Focus Tree",
+    address:
+      "0x0377c2d65debb3978ea81904e7d59740da1f07412e30d01c5ded1c5d6f1ddc43",
+    image: "/collections/0cf88fb6-cbc4-416e-8fa7-1b8bcafc610b.png",
+  },
+  {
+    name: "WeebPunks",
+    address:
+      "0x0507e93cecf4e2b5b5cc6a9131db8dffb17f32a8c7bd5fe1011baa290ed7dcbb",
+    image: "/collections/20240223224948e34066914831407683fa5aec15fffade.png",
+  },
+  {
+    name: "Quantum Leap",
+    address:
+      "0x00b719f69b00a008a797dc48585449730aa1c09901fdbac1bc94b3bdc287cf76",
+    image: "/collections/54bae180-0516-4635-a249-6e2add00bc7e.png",
+  },
+  {
+    name: "Starkurabu Traits",
+    address:
+      "0x02fd2dc038802e5282b27974cc2a937f79ea5762d2c195aeff41f1fd57da1ca8",
+    image: "/collections/7d9903ba-ec50-4474-b2b1-7eb4fbc264bd.png",
+  },
+  {
+    name: "Stark Fox Club",
+    address:
+      "0x03b0774705a2ca356347181884569cd810155c1b9e8de9ec15d93410cc1e94c3",
+    image: "/collections/2024020519005746c96cca631946d389779db31a4014fc.gif",
+  },
+  {
+    name: "Starkpunks",
+    address:
+      "0x0727a63f78ee3f1bd18f78009067411ab369c31dece1ae22e16f567906409905",
+    image: "/collections/13041814-9ee2-4d22-b060-ad6de5a3dedc.webp",
+  },
+  {
+    name: "Ducks Everywhere",
+    address:
+      "0x04fa864a706e3403fd17ac8df307f22eafa21b778b73353abf69a622e47a2003",
+    image: "/collections/46a33d89-7a73-46a9-93f1-ccca5e386332.png",
+  },
+  {
+    name: "Metacube: Passcards",
+    address:
+      "0x0602c301f6a1c2ef174bafaab7389c3f6165df34736befcf2ca3df7764934caf",
+    image: "/collections/20240602033225e51b51e85e60446eaa5f53421b0ebd98.gif",
+  },
+];
