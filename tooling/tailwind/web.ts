69da4a3e6d0e72aa044626e0161df278976973bc
paul launay <launay.p@gmail.com>
2024-06-25 15:38:11 +0200

    feat: add missing radius

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -19,6 +19,7 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        xs: "calc(var(--radius) - 6px)",
       },
       keyframes: {
         "accordion-down": {
