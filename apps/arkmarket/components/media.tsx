d881b6db228fb7b8ad53bcba5c282f66a94986df
Yohan Tancrez <yohantancrez@gmail.com>
2024-06-25 03:15:14 +0200

    eager prop

--- apps/arkmarket/components/media.tsx
+++ apps/arkmarket/components/media.tsx
@@ -132,7 +132,7 @@ export default function Media({
         />
       )}
       <Image
-        // loading="eager"
+        loading="eager"
         unoptimized
         alt={alt}
         className={cn(
