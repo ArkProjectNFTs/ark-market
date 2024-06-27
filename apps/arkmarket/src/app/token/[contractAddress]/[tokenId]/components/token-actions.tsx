dec18714d13ee7ae042f5c8c20dd0cdd003d48e0
Yohan Tancrez <yohantancrez@gmail.com>
2024-06-27 11:14:58 +0200

    feat: spacings

--- apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions.tsx
+++ apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions.tsx
@@ -35,14 +35,14 @@ export default function TokenActions({
         )}
       >
         <div className="flex flex-col-reverse gap-6 font-medium text-muted-foreground lg:flex-row lg:items-center lg:justify-between lg:gap-0">
-          <p>Best Price</p>
+          <p className="text-sm lg:text-base">Best Price</p>
           <div className="flex items-center gap-1.5">
             <TimerReset />
             <p>Time Left 12d 13h 45m</p>
           </div>
         </div>
 
-        <div className="mt-2 flex items-center justify-between gap-6 lg:mt-4 lg:justify-start">
+        <div className="mt-1.5 flex items-center justify-between gap-6 lg:mt-4 lg:justify-start">
           <div className="flex items-center gap-3 lg:gap-6">
             <p
               className={cn(
@@ -62,7 +62,7 @@ export default function TokenActions({
         </div>
 
         <div
-          className="mt-9 flex flex-col items-center gap-4 lg:flex-row lg:gap-8"
+          className="mt-6 flex flex-col items-center gap-4 lg:mt-9 lg:flex-row lg:gap-8"
           ref={ref}
         >
           <Button className="relative w-full" size="xl">
