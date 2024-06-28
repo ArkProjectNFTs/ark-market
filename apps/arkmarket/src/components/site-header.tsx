5bbd2d24a6d584bcf66908924f54381f1964a6e1
Yohan Tancrez <yohantancrez@gmail.com>
2024-06-28 03:16:11 +0200

    home page

--- apps/arkmarket/src/components/site-header.tsx
+++ apps/arkmarket/src/components/site-header.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 
 import { Input } from "@ark-market/ui/input";
 
-// import { ThemeToggle } from "@ark-market/ui/theme";
-
-// import { CommandMenu } from "~/components/command-menu";
 import { MainNav } from "~/components/main-nav";
 import { UserNav } from "~/components/user-nav";
 
@@ -13,7 +10,6 @@ export default function SiteHeader() {
     <div className="sticky top-0 z-20 flex h-[var(--site-header-height)] w-full items-center justify-between bg-background px-5 lg:border-b">
       <MainNav />
       <div className="w-full flex-1 md:w-auto md:flex-none">
-        {/* <CommandMenu /> */}
         <Input
           placeholder="Search Nft, collections and account"
           className="hidden w-80 lg:block"
@@ -21,7 +17,6 @@ export default function SiteHeader() {
       </div>
       <div className="flex items-center space-x-4">
         <UserNav />
-        {/* <ThemeToggle /> */}
       </div>
     </div>
   );
--- apps/arkmarket/src/config/homepage.ts
+++ apps/arkmarket/src/config/homepage.ts
@@ -1,212 +1,252 @@
-export const collections = [
-  {
-    name: "Blobert",
-    address:
-      "0x00539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1",
-    image: "/collections/c6fc5552-1051-4f68-87c9-fcd6ddc1f026.jpeg",
-  },
-  {
-    name: "Grails",
-    address:
-      "0x02a819b93cc69b45ee5d1a1bfc16954c16f6d35c3873a06c97b95c009bfe502b",
-    image: "/collections/grail.png",
-  },
-  {
-    name: "Starknet.id",
-    address:
-      "0x05dbdedc203e92749e2e746e2d40a768d966bd243df04a6b712e222bc040a9af",
-    image: "/collections/59685ea9-aeae-4784-82a1-5aa9364cea9b.png",
-  },
-  {
-    name: "Alter Nova",
-    address:
-      "0x0169e971d146ccf8f5e88f2b12e2e6099663fb56e42573479f2aee93309982f8",
-    image: "/collections/2024030811331141bb5ac7adfa420ab24b813ece99b01a.gif",
-  },
-  {
-    name: "Starknet Quest",
-    address:
-      "0x076503062d78f4481be03c9145022d6a4a71ec0719aa07756f79a2384dc7ef16",
-    image: "/collections/fff43877-5668-4392-bced-f95f6faa8dd8.png",
-  },
-  {
-    name: "Argent: Xplorer",
-    address:
-      "0x01b22f7a9d18754c994ae0ee9adb4628d414232e3ebd748c386ac286f86c3066",
-    image: "/collections/eee0d689-bf44-4a77-bdbe-eeeceb6d4b64.png",
-  },
-  {
-    name: "carbonable - KARATHURU",
-    address:
-      "0x05a667adc04676fba78a29371561a0bf91dab25847d5dc4709a93a4cfb5ff293",
-    image: "/collections/carbonable.jpeg",
-  },
-  {
-    name: "Flex storefront",
-    address:
-      "0x02e6c908da3d1ced80d81085ed9374b7c5048f86799e1f35e54daca4d70832d7",
-    image: "/collections/flex-store.png",
-  },
-  {
-    name: "Flex evo",
-    address:
-      "0x04546729db564bb29a9e1e215463f41bc53116ac75eeb8e029b8a87fee7d85fd",
-    image: "/collections/flex-evo.jpg",
-  },
-  {
-    name: "Influence asteroids",
-    address:
-      "0x0603cf837055c64d026a3c5a9e3a83036cea6c4a3f68a9e19f7a687d726fe817",
-    image: "/collections/influence.png",
-  },
-  {
-    name: "Rise of the First LPs",
-    address:
-      "0x05b991e122ff2410d32575eb369059bd26cb69a05d15001a0c93f2678d96d81c",
-    image: "/collections/rise.webp",
-  },
-  {
-    name: "Ventoria Crystals - Ruby Flame",
-    address:
-      "0x00125e36efcba872e578b6955ce08c686b10e1d22ac01de8eb5b2d51488f743a",
-    image: "/collections/ventory.png",
-  },
-  {
-    name: "Mad ape club",
-    address:
-      "0x01335defa219c2d0911a75ff85245200932501d6975d6178eabfa544e7d82b07",
-    image: "/collections/mad-ape.png",
-  },
-  {
-    name: "Pyramid store",
-    address:
-      "0x42e7815d9e90b7ea53f4550f74dc12207ed6a0faaef57ba0dbf9a66f3762d82",
-    image: "/collections/pyramid.png",
-  },
-  {
-    name: "Lotus Project",
-    address:
-      "0x61ccf82a5628c0dbe3d0cc10e904882f1e9bc8adbb9456bdba155341bf430e5",
-    image: "/collections/lotus.webp",
-  },
-  {
-    name: "Fibrous Arena",
-    address:
-      "0x1d2eac1bcddde10b595fd4e6247aa105ec69fef308c94fe612f435fe5798d19",
-    image: "/collections/fibrous.webp",
-  },
-  {
-    name: "The starks",
-    address:
-      "0x0485209349294398d62ad18849b8ec0940ab7fe63117fb59eba2ab7446d78394",
-    image: "/collections/the-starks.webp",
-  },
-  {
-    name: "Stonk",
-    address:
-      "0x076770d3d367bb07af81b9617216d1e1cf1fa1526104cb09686994825283090f",
-    image: "/collections/stonk.png",
-  },
-  {
-    name: "Ventorians",
-    address:
-      "0x064f76d32edd460a30b4acf62bfb2e0e76f450d61994ab698224f0fe6ae72ee5",
-    image: "/collections/ventorians.png",
-  },
-  {
-    name: "Dragark Genesis Collection",
-    address:
-      "0x051d0844f96f86c7363cc7eb3ab939e0ef5b70939dcbc17895b2fa178d9af420",
-    image: "/collections/dragark.png",
-  },
-  {
-    name: "Starkguardians",
-    address:
-      "0x02d679a171589777bc996fb27767ff9a2e44c7e07967760dea3df31704ab398a",
-    image: "/collections/starguardians.webp",
-  },
-  {
-    name: "Metacube: Genesis",
-    address:
-      "0x007ca74fd0a9239678cc6355e38ac1e7820141501727ae37f9c733e5ed1c3592",
-    image: "/collections/metacube-genesis.gif",
-  },
-  {
-    name: "Dreambotty",
-    address:
-      "0x3859bf9178b48a4ba330d6872ab5a6d3895b64d6631197beefde6293bc172cd",
-    image: "/collections/dreambotty.webp",
-  },
-  {
-    name: "The Crown of Stark",
-    address:
-      "0x041302e7d6c1329ceb18df33107ff285e3a46329f281aba2b0eb957240649760",
-    image: "/collections/6fe90ed7-7b32-4a0d-a42b-bc316873112c.jpg",
-  },
-  {
-    name: "Dragark Element NFT",
-    address:
-      "0x0537fa10cefbb8ff7e61e86e950746809f95faa7398f250f0063069b29bb7933",
-    image: "/collections/1.png",
-  },
-  {
-    name: "briq Sets",
-    address:
-      "0x01435498bf393da86b4733b9264a86b58a42b31f8d8b8ba309593e5c17847672",
-    image: "/collections/b0c8387c-08cf-4b7d-8c5a-4d743bfcc727.png",
-  },
-  {
-    name: "Spok",
-    address:
-      "0x07606cac9053e9b8b573a4b0a0ce608880f64869e24b8a605210d7a85bb6e5f1",
-    image: "/collections/522ae053-3dfa-4305-8bbe-2381d581cb9b.png",
-  },
-  {
-    name: "Starkurabu",
-    address:
-      "0x03ab1124ef9ec3a2f2b1d9838f9066f9a894483d40b33390dda8d85c01a315a3",
-    image: "/collections/7d9903ba-ec50-4474-b2b1-7eb4fbc264bd.png",
-  },
-  {
-    name: "StarkRock",
-    address:
-      "0x012f8e318fe04a1fe8bffe005ea4bbd19cb77a656b4f42682aab8a0ed20702f0",
-    image: "/collections/f90e79fe-2477-461c-a4df-4f62c4134a33.png",
-  },
-  {
-    name: "Focus Tree",
-    address:
-      "0x0377c2d65debb3978ea81904e7d59740da1f07412e30d01c5ded1c5d6f1ddc43",
-    image: "/collections/0cf88fb6-cbc4-416e-8fa7-1b8bcafc610b.png",
-  },
-  {
-    name: "WeebPunks",
-    address:
-      "0x0507e93cecf4e2b5b5cc6a9131db8dffb17f32a8c7bd5fe1011baa290ed7dcbb",
-    image: "/collections/20240223224948e34066914831407683fa5aec15fffade.png",
-  },
-  {
-    name: "Quantum Leap",
-    address:
-      "0x00b719f69b00a008a797dc48585449730aa1c09901fdbac1bc94b3bdc287cf76",
-    image: "/collections/54bae180-0516-4635-a249-6e2add00bc7e.png",
-  },
-  {
-    name: "Starkpunks",
-    address:
-      "0x0727a63f78ee3f1bd18f78009067411ab369c31dece1ae22e16f567906409905",
-    image: "/collections/13041814-9ee2-4d22-b060-ad6de5a3dedc.webp",
-  },
-  {
-    name: "Ducks Everywhere",
-    address:
-      "0x04fa864a706e3403fd17ac8df307f22eafa21b778b73353abf69a622e47a2003",
-    image: "/collections/46a33d89-7a73-46a9-93f1-ccca5e386332.png",
-  },
-  {
-    name: "Metacube: Passcards",
-    address:
-      "0x0602c301f6a1c2ef174bafaab7389c3f6165df34736befcf2ca3df7764934caf",
-    image: "/collections/20240602033225e51b51e85e60446eaa5f53421b0ebd98.gif",
-  },
-];
+export const homepageConfig = {
+  latestDropCollections: [
+    { name: "Collection name" },
+    { name: "Collection name" },
+    { name: "Collection name" },
+    { name: "Collection name" },
+  ],
+  trendingCollections: [
+    {
+      name: "Grails",
+      address:
+        "0x02a819b93cc69b45ee5d1a1bfc16954c16f6d35c3873a06c97b95c009bfe502b",
+      image: "/collections/grail.png",
+    },
+    {
+      name: "Starknet Quest",
+      address:
+        "0x076503062d78f4481be03c9145022d6a4a71ec0719aa07756f79a2384dc7ef16",
+      image: "/collections/fff43877-5668-4392-bced-f95f6faa8dd8.png",
+    },
+    {
+      name: "Argent: Xplorer",
+      address:
+        "0x01b22f7a9d18754c994ae0ee9adb4628d414232e3ebd748c386ac286f86c3066",
+      image: "/collections/eee0d689-bf44-4a77-bdbe-eeeceb6d4b64.png",
+    },
+    {
+      name: "Starknet.id",
+      address:
+        "0x05dbdedc203e92749e2e746e2d40a768d966bd243df04a6b712e222bc040a9af",
+      image: "/collections/59685ea9-aeae-4784-82a1-5aa9364cea9b.png",
+    },
+  ],
+  exploreCollections: [
+    {
+      name: "Everai",
+      address:
+        "0x02acee8c430f62333cf0e0e7a94b2347b5513b4c25f699461dd8d7b23c072478",
+      image: "/collections/everai.png",
+      banner_image: "/banners/everai.png",
+    },
+    {
+      name: "Starkurabu",
+      address:
+        "0x03ab1124ef9ec3a2f2b1d9838f9066f9a894483d40b33390dda8d85c01a315a3",
+      image: "/collections/7d9903ba-ec50-4474-b2b1-7eb4fbc264bd.png",
+      banner_image: "/banners/starkurabu.png",
+    },
+    {
+      name: "Blobert",
+      address:
+        "0x00539f522b29ae9251dbf7443c7a950cf260372e69efab3710a11bf17a9599f1",
+      image: "/collections/c6fc5552-1051-4f68-87c9-fcd6ddc1f026.jpeg",
+      banner_image: "/banners/blobert.png",
+    },
+    {
+      name: "Ducks Everywhere",
+      address:
+        "0x04fa864a706e3403fd17ac8df307f22eafa21b778b73353abf69a622e47a2003",
+      image: "/collections/46a33d89-7a73-46a9-93f1-ccca5e386332.png",
+      banner_image: "/banners/ducks.png",
+    },
+    {
+      name: "briq Sets",
+      address:
+        "0x01435498bf393da86b4733b9264a86b58a42b31f8d8b8ba309593e5c17847672",
+      image: "/collections/b0c8387c-08cf-4b7d-8c5a-4d743bfcc727.png",
+      banner_image: "/banners/briq.png",
+    },
+    {
+      name: "Alter Nova",
+      address:
+        "0x0169e971d146ccf8f5e88f2b12e2e6099663fb56e42573479f2aee93309982f8",
+      image: "/collections/2024030811331141bb5ac7adfa420ab24b813ece99b01a.gif",
+      banner_image: "/banners/alternova.png",
+    },
+    {
+      name: "Grails",
+      address:
+        "0x02a819b93cc69b45ee5d1a1bfc16954c16f6d35c3873a06c97b95c009bfe502b",
+      image: "/collections/grail.png",
+    },
+    {
+      name: "Argent: Xplorer",
+      address:
+        "0x01b22f7a9d18754c994ae0ee9adb4628d414232e3ebd748c386ac286f86c3066",
+      image: "/collections/eee0d689-bf44-4a77-bdbe-eeeceb6d4b64.png",
+    },
+    {
+      name: "Carbonable - KARATHURU",
+      address:
+        "0x05a667adc04676fba78a29371561a0bf91dab25847d5dc4709a93a4cfb5ff293",
+      image: "/collections/carbonable.jpeg",
+    },
+    {
+      name: "Flex storefront",
+      address:
+        "0x02e6c908da3d1ced80d81085ed9374b7c5048f86799e1f35e54daca4d70832d7",
+      image: "/collections/flex-store.png",
+    },
+    {
+      name: "Flex evo",
+      address:
+        "0x04546729db564bb29a9e1e215463f41bc53116ac75eeb8e029b8a87fee7d85fd",
+      image: "/collections/flex-evo.jpg",
+    },
+    {
+      name: "Influence asteroids",
+      address:
+        "0x0603cf837055c64d026a3c5a9e3a83036cea6c4a3f68a9e19f7a687d726fe817",
+      image: "/collections/influence.png",
+    },
+    {
+      name: "Rise of the First LPs",
+      address:
+        "0x05b991e122ff2410d32575eb369059bd26cb69a05d15001a0c93f2678d96d81c",
+      image: "/collections/rise.webp",
+    },
+    {
+      name: "Ventoria Crystals - Ruby Flame",
+      address:
+        "0x00125e36efcba872e578b6955ce08c686b10e1d22ac01de8eb5b2d51488f743a",
+      image: "/collections/ventory.png",
+    },
+    {
+      name: "Mad ape club",
+      address:
+        "0x01335defa219c2d0911a75ff85245200932501d6975d6178eabfa544e7d82b07",
+      image: "/collections/mad-ape.png",
+    },
+    {
+      name: "Pyramid store",
+      address:
+        "0x42e7815d9e90b7ea53f4550f74dc12207ed6a0faaef57ba0dbf9a66f3762d82",
+      image: "/collections/pyramid.png",
+    },
+    {
+      name: "Lotus Project",
+      address:
+        "0x61ccf82a5628c0dbe3d0cc10e904882f1e9bc8adbb9456bdba155341bf430e5",
+      image: "/collections/lotus.webp",
+    },
+    {
+      name: "Fibrous Arena",
+      address:
+        "0x1d2eac1bcddde10b595fd4e6247aa105ec69fef308c94fe612f435fe5798d19",
+      image: "/collections/fibrous.webp",
+    },
+    {
+      name: "The starks",
+      address:
+        "0x0485209349294398d62ad18849b8ec0940ab7fe63117fb59eba2ab7446d78394",
+      image: "/collections/the-starks.webp",
+    },
+    {
+      name: "Stonk",
+      address:
+        "0x076770d3d367bb07af81b9617216d1e1cf1fa1526104cb09686994825283090f",
+      image: "/collections/stonk.png",
+    },
+    {
+      name: "Ventorians",
+      address:
+        "0x064f76d32edd460a30b4acf62bfb2e0e76f450d61994ab698224f0fe6ae72ee5",
+      image: "/collections/ventorians.png",
+    },
+    {
+      name: "Dragark Genesis Collection",
+      address:
+        "0x051d0844f96f86c7363cc7eb3ab939e0ef5b70939dcbc17895b2fa178d9af420",
+      image: "/collections/dragark.png",
+    },
+    {
+      name: "Starkguardians",
+      address:
+        "0x02d679a171589777bc996fb27767ff9a2e44c7e07967760dea3df31704ab398a",
+      image: "/collections/starguardians.webp",
+    },
+    {
+      name: "Metacube: Genesis",
+      address:
+        "0x007ca74fd0a9239678cc6355e38ac1e7820141501727ae37f9c733e5ed1c3592",
+      image: "/collections/metacube-genesis.gif",
+    },
+    {
+      name: "Dreambotty",
+      address:
+        "0x3859bf9178b48a4ba330d6872ab5a6d3895b64d6631197beefde6293bc172cd",
+      image: "/collections/dreambotty.webp",
+    },
+    {
+      name: "The Crown of Stark",
+      address:
+        "0x041302e7d6c1329ceb18df33107ff285e3a46329f281aba2b0eb957240649760",
+      image: "/collections/6fe90ed7-7b32-4a0d-a42b-bc316873112c.jpg",
+    },
+    {
+      name: "Dragark Element NFT",
+      address:
+        "0x0537fa10cefbb8ff7e61e86e950746809f95faa7398f250f0063069b29bb7933",
+      image: "/collections/1.png",
+    },
+    {
+      name: "Spok",
+      address:
+        "0x07606cac9053e9b8b573a4b0a0ce608880f64869e24b8a605210d7a85bb6e5f1",
+      image: "/collections/522ae053-3dfa-4305-8bbe-2381d581cb9b.png",
+    },
+    {
+      name: "Starkurabu",
+      address:
+        "0x03ab1124ef9ec3a2f2b1d9838f9066f9a894483d40b33390dda8d85c01a315a3",
+      image: "/collections/7d9903ba-ec50-4474-b2b1-7eb4fbc264bd.png",
+    },
+    {
+      name: "StarkRock",
+      address:
+        "0x012f8e318fe04a1fe8bffe005ea4bbd19cb77a656b4f42682aab8a0ed20702f0",
+      image: "/collections/f90e79fe-2477-461c-a4df-4f62c4134a33.png",
+    },
+    {
+      name: "Focus Tree",
+      address:
+        "0x0377c2d65debb3978ea81904e7d59740da1f07412e30d01c5ded1c5d6f1ddc43",
+      image: "/collections/0cf88fb6-cbc4-416e-8fa7-1b8bcafc610b.png",
+    },
+    {
+      name: "WeebPunks",
+      address:
+        "0x0507e93cecf4e2b5b5cc6a9131db8dffb17f32a8c7bd5fe1011baa290ed7dcbb",
+      image: "/collections/20240223224948e34066914831407683fa5aec15fffade.png",
+    },
+    {
+      name: "Quantum Leap",
+      address:
+        "0x00b719f69b00a008a797dc48585449730aa1c09901fdbac1bc94b3bdc287cf76",
+      image: "/collections/54bae180-0516-4635-a249-6e2add00bc7e.png",
+    },
+    {
+      name: "Starkpunks",
+      address:
+        "0x0727a63f78ee3f1bd18f78009067411ab369c31dece1ae22e16f567906409905",
+      image: "/collections/13041814-9ee2-4d22-b060-ad6de5a3dedc.webp",
+    },
+    {
+      name: "Metacube: Passcards",
+      address:
+        "0x0602c301f6a1c2ef174bafaab7389c3f6165df34736befcf2ca3df7764934caf",
+      image: "/collections/20240602033225e51b51e85e60446eaa5f53421b0ebd98.gif",
+    },
+  ],
+};
