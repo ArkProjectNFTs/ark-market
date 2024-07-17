1802437c83dbf09354b903a2fff4532f94c9e90b
YohanTz <yohantancrez@gmail.com>
2024-07-17 11:16:16 +0200

    feat: updated error input state

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -16,7 +16,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "flex h-12 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
-          status === "error" ? "border-destructive" : "border-input",
+          status === "error"
+            ? "border-destructive focus-visible:border-input"
+            : "border-input",
           className,
         )}
         ref={ref}
