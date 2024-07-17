7acf94e1b95d9806d9b0e6c76e2f8eb1e9fcd80e
paul launay <launay.p@gmail.com>
2024-07-17 19:06:39 +0200

    feat: add sticky empty action bar

--- apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-empty.tsx
+++ apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-empty.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import { Meh } from "lucide-react";
 
 import type { Token } from "~/types";
+import TokenActionsBarEmpty from "./token-actions-bar-empty";
 import { TokenActionsCreateListing } from "./token-actions-create-listing";
 import TokenActionsMakeOffer from "./token-actions-make-offer";
 
@@ -15,21 +18,33 @@ export default function TokenActionsEmpty({
   token,
   isOwner,
 }: TokenActionsEmptyProps) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isActionItemsInView = useInView(ref, { margin: "-72px 0px 0px 0px" });
+
   return (
-    <div className="rounded-lg bg-card p-5 text-center lg:px-8 lg:pb-10 lg:pt-8">
-      <div className="flex flex-col items-center pb-8 text-muted-foreground">
-        <Meh size={42} className="flex-shrink-0" />
-        <p className="mt-3 text-center text-xl font-semibold">
-          {isOwner
-            ? "You have not assigned a price to your token yet."
-            : "This token has not been listed yet."}
-        </p>
+    <>
+      <TokenActionsBarEmpty
+        token={token}
+        isOwner={isOwner}
+        show={!isActionItemsInView}
+      />
+      <div className="rounded-lg bg-card p-5 text-center lg:px-8 lg:pb-10 lg:pt-8">
+        <div className="flex flex-col items-center pb-8 text-muted-foreground">
+          <Meh size={42} className="flex-shrink-0" />
+          <p className="mt-3 text-center text-xl font-semibold">
+            {isOwner
+              ? "You have not assigned a price to your token yet."
+              : "This token has not been listed yet."}
+          </p>
+        </div>
+        <div ref={ref}>
+          {isOwner ? (
+            <TokenActionsCreateListing token={token} />
+          ) : (
+            <TokenActionsMakeOffer token={token} />
+          )}
+        </div>
       </div>
-      {isOwner ? (
-        <TokenActionsCreateListing token={token} />
-      ) : (
-        <TokenActionsMakeOffer token={token} />
-      )}
-    </div>
+    </>
   );
 }
