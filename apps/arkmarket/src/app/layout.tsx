4d651b37f70567c18806992f29ea23f046c056b0
paul launay <launay.p@gmail.com>
2024-08-27 03:12:15 +0200

    fix(arkmarket): update viewport meta tag for responsive scaling and layout control

--- apps/arkmarket/src/app/layout.tsx
+++ apps/arkmarket/src/app/layout.tsx
@@ -44,6 +44,12 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: "device-width",
+  height: "device-height",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
