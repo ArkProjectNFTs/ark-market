417baa9b24c2e466aaf84196edceedd2f17e8986
kwiss <mr.dumont.christophe@gmail.com>
2024-09-02 23:38:25 +0200

    fix: remove icon folder

--- packages/ui/src/icons/starknet-logo-2.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithClassName } from "..";
-import { Icon } from "./icon";
-
-export default function StarknetLogo2({ className }: PropsWithClassName) {
-  return <Icon className={className} icon="Y" />;
-}
--- packages/ui/src/icons/starknet-logo.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithClassName } from "..";
-import { Icon } from "./icon";
-
-export default function StarknetLogo({ className }: PropsWithClassName) {
-  return <Icon className={className} icon="Y" />;
-}
