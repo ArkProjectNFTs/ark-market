be54804c2a90b08dff1cfb33f1ef7edb48af448f
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-09-04 19:14:53 +0200

    feat(token): refresh token metadata (#113)

---
+++ apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/refresh-metadata-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { RefreshCw } from "lucide-react";
+
+import { useToast } from "@ark-market/ui/use-toast";
+
+import postTokenMetadataRefresh from "~/lib/postTokenMetadataRefresh";
+
+interface RefreshMetadataButtonProps {
+  contractAddress: string;
+  tokenId: string;
+}
+
+export default function RefreshMetadataButton({
+  contractAddress,
+  tokenId,
+}: RefreshMetadataButtonProps) {
+  const { toast } = useToast();
+
+  const refreshMetadataMutation = useMutation({
+    mutationFn: async () => {
+      await postTokenMetadataRefresh({
+        contractAddress,
+        tokenId,
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: "Metadata is refreshing",
+        description:
+          "We've queued this item for an update! Check back in a minute...",
+      });
+    },
+    onError: () => {
+      toast({
+        title: "Failed to refresh metadata",
+        description: "Please try again later.",
+      });
+    },
+  });
+
+  return (
+    <button
+      onClick={async () => {
+        await refreshMetadataMutation.mutateAsync();
+      }}
+    >
+      <RefreshCw className="size-6 text-muted-foreground" />
+    </button>
+  );
+}
