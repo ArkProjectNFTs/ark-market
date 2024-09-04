cc2943341057fe89b8f02b242baf864ea8480c08
Paul Launay <launay.p@gmail.com>
2024-09-04 02:45:14 +0200

    feat(arkmarket): hide selected trait filters on mobile (#119)

--- apps/arkmarket/src/app/collection/[collectionAddress]/components/collection-items-filters-traits.tsx
+++ apps/arkmarket/src/app/collection/[collectionAddress]/components/collection-items-filters-traits.tsx
@@ -1,6 +1,8 @@
 import { X } from "lucide-react";
 
-interface CollectionNavProps {
+import { Button } from "@ark-market/ui/button";
+
+interface CollectionItemsFiltersTraitsProps {
   traits: Record<string, string[]>;
   onRemove: (key: string, value: string) => Promise<void>;
   onReset: () => Promise<void>;
@@ -10,36 +12,35 @@ export default function CollectionItemsFiltersTraits({
   traits,
   onRemove,
   onReset,
-}: CollectionNavProps) {
+}: CollectionItemsFiltersTraitsProps) {
   if (Object.keys(traits).length === 0) {
     return null;
   }
 
   return (
-    <div className="flex flex-wrap items-center gap-4 px-5 pb-5">
-      {Object.keys(traits).map((key) => (
-        <>
-          {traits[key]?.map((value) => (
-            <div
-              key={`${key}-${value}`}
-              className="flex cursor-pointer items-center gap-2 rounded-md bg-muted px-4 py-3"
-              onClick={() => onRemove(key, value)}
-            >
-              <div className="text-sm leading-none">
-                {key}: {value}
-              </div>
-              <X className="size-4" />
+    <div className="hidden flex-wrap items-center gap-4 px-5 pb-5 lg:flex">
+      {Object.keys(traits).map((key) =>
+        traits[key]?.map((value) => (
+          <Button
+            key={`${key}-${value}`}
+            className="flex cursor-pointer items-center gap-2 rounded-md bg-muted px-4 py-3"
+            variant="outline"
+            onClick={() => onRemove(key, value)}
+          >
+            <div className="text-sm leading-none">
+              {key}: {value}
             </div>
-          ))}
-        </>
-      ))}
-
-      <div
+            <X className="size-4" />
+          </Button>
+        )),
+      )}
+      <Button
         className="flex cursor-pointer items-center gap-2 rounded-md bg-muted px-4 py-3 text-sm leading-none"
+        variant="outline"
         onClick={onReset}
       >
         Clear all
-      </div>
+      </Button>
     </div>
   );
 }
