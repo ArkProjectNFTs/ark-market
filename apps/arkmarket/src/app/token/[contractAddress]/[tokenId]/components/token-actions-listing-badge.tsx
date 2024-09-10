d709aea7480dd491374049a32df6980a0b08a4d3
Yohan Tancrez <yohantancrez@gmail.com>
2024-09-10 21:25:43 +0200

    feat: proper tag color

--- apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-listing-badge.tsx
+++ apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-listing-badge.tsx
@@ -1,9 +1,13 @@
 import { cn } from "@ark-market/ui";
 
+import { env } from "~/env";
+
 interface ListingBadgeProps {
   isListed: boolean;
   isAuction: boolean;
 }
+
+const isUnframed = env.NEXT_PUBLIC_THEME === "unframed";
 
 export default function TokenActionsListingBadge({
   isListed,
@@ -15,9 +19,15 @@ export default function TokenActionsListingBadge({
         "w-fit rounded-xl border border-blue-500 px-4 py-2 text-sm text-blue-500",
         isListed
           ? isAuction
-            ? "border-green-500 text-green-500"
-            : "border-blue-500 text-blue-500"
-          : "border-orange-500 text-orange-500",
+            ? isUnframed
+              ? "border-primary text-primary"
+              : "border-green-500 text-green-500"
+            : isUnframed
+              ? "border-primary text-primary"
+              : "border-blue-500 text-blue-500"
+          : isUnframed
+            ? "border-[#CCD0DB] text-[#CCD0DB]"
+            : "border-orange-500 text-orange-500",
       )}
     >
       {isListed ? (isAuction ? "Up for auction" : "For sale") : "Not for sale"}
