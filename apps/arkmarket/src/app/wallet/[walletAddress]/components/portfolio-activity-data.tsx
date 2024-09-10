af887acca4d75596e28eb568a3994139da7ccdc7
Yohan @ ScreenshotLabs <yohantancrez@gmail.com>
2024-09-10 21:02:33 +0200

    feat: added different empty states (#132)

--- apps/arkmarket/src/app/wallet/[walletAddress]/components/portfolio-activity-data.tsx
+++ apps/arkmarket/src/app/wallet/[walletAddress]/components/portfolio-activity-data.tsx
@@ -13,7 +13,7 @@ import {
   timeSince,
 } from "@ark-market/ui";
 import { Button } from "@ark-market/ui/button";
-import { ArrowUpRight, VerifiedIcon } from "@ark-market/ui/icons";
+import { ArrowUpRight, NoActivity, VerifiedIcon } from "@ark-market/ui/icons";
 import { PriceTag } from "@ark-market/ui/price-tag";
 import {
   Table,
@@ -90,151 +90,159 @@ export default function PortfolioActivityData({
   });
 
   return (
-    <Table ref={tableRef}>
-      <TableHeader className="h-12">
-        <TableRow
-          className={cn(
-            "absolute grid w-full items-center",
-            gridTemplateColumnValue,
-          )}
+    <>
+      <Table ref={tableRef}>
+        <TableHeader className="h-12">
+          <TableRow
+            className={cn(
+              "absolute grid w-full items-center",
+              gridTemplateColumnValue,
+            )}
+          >
+            <TableHead className="sticky top-0 flex items-center bg-background pl-5">
+              Event
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Token
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Price
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              From
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              To
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Date
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background"></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          className="font-numbers relative text-sm font-medium"
+          style={{ height: `${rowVirtualizer.getTotalSize() + 2}px` }}
         >
-          <TableHead className="sticky top-0 flex items-center bg-background pl-5">
-            Event
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Token
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Price
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            From
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            To
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Date
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody
-        className="font-numbers relative text-sm font-medium"
-        style={{ height: `${rowVirtualizer.getTotalSize() + 2}px` }}
-      >
-        {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-          const activity = portfolioActivity[virtualRow.index];
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const activity = portfolioActivity[virtualRow.index];
 
-          if (activity === undefined) {
-            return null;
-          }
+            if (activity === undefined) {
+              return null;
+            }
 
-          return (
-            <TableRow
-              className={cn(
-                "group absolute grid h-[6.25rem] w-full items-center",
-                gridTemplateColumnValue,
-              )}
-              data-index={virtualRow.index}
-              key={`${virtualRow.index}-${activity.time_stamp}-${activity.transaction_hash}`}
-              ref={(node) => rowVirtualizer.measureElement(node)}
-              style={{
-                transform: `translateY(${virtualRow.start}px)`,
-              }}
-            >
-              <TableCell className="items-center gap-4 whitespace-nowrap pl-5">
-                <div className="flex items-center gap-4 whitespace-nowrap">
-                  {activityTypeMetadata[activity.activity_type].icon}
-                  <p>{activityTypeMetadata[activity.activity_type].title}</p>
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="flex items-center gap-4">
-                  <Media
-                    className="size-[3.75rem] rounded-xs object-contain"
-                    height={120}
-                    width={120}
-                    alt={activity.metadata?.name ?? "Unnamed Token"}
-                    src={activity.metadata?.image ?? ""}
-                    mediaKey={activity.metadata?.image_key ?? ""}
-                  />
-                  <div className="w-full overflow-hidden">
-                    <Link
-                      className={focusableStyles}
-                      href={`/token/${activity.collection_address}/${activity.token_id}`}
-                    >
-                      <p
-                        className={cn(
-                          "w-full text-base font-medium",
-                          ellipsableStyles,
-                        )}
-                      >
-                        {activity.metadata?.name ?? "Unnamed Token"}
-                      </p>
-                    </Link>
-                    <div className="flex w-full items-center gap-1">
+            return (
+              <TableRow
+                className={cn(
+                  "group absolute grid h-[6.25rem] w-full items-center",
+                  gridTemplateColumnValue,
+                )}
+                data-index={virtualRow.index}
+                key={`${virtualRow.index}-${activity.time_stamp}-${activity.transaction_hash}`}
+                ref={(node) => rowVirtualizer.measureElement(node)}
+                style={{
+                  transform: `translateY(${virtualRow.start}px)`,
+                }}
+              >
+                <TableCell className="items-center gap-4 whitespace-nowrap pl-5">
+                  <div className="flex items-center gap-4 whitespace-nowrap">
+                    {activityTypeMetadata[activity.activity_type].icon}
+                    <p>{activityTypeMetadata[activity.activity_type].title}</p>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex items-center gap-4">
+                    <Media
+                      className="size-[3.75rem] rounded-xs object-contain"
+                      height={120}
+                      width={120}
+                      alt={activity.metadata?.name ?? "Unnamed Token"}
+                      src={activity.metadata?.image ?? ""}
+                      mediaKey={activity.metadata?.image_key ?? ""}
+                    />
+                    <div className="w-full overflow-hidden">
                       <Link
                         className={focusableStyles}
-                        href={`/collection/${activity.collection_address}`}
+                        href={`/token/${activity.collection_address}/${activity.token_id}`}
                       >
                         <p
                           className={cn(
-                            "text-muted-foreground",
+                            "w-full text-base font-medium",
                             ellipsableStyles,
                           )}
                         >
-                          {activity.collection_name}
+                          {activity.metadata?.name ?? "Unnamed Token"}
                         </p>
                       </Link>
-                      {activity.collection_is_verified && (
-                        <VerifiedIcon className="size-4 text-primary" />
-                      )}
+                      <div className="flex w-full items-center gap-1">
+                        <Link
+                          className={focusableStyles}
+                          href={`/collection/${activity.collection_address}`}
+                        >
+                          <p
+                            className={cn(
+                              "text-muted-foreground",
+                              ellipsableStyles,
+                            )}
+                          >
+                            {activity.collection_name}
+                          </p>
+                        </Link>
+                        {activity.collection_is_verified && (
+                          <VerifiedIcon className="size-4 text-primary" />
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </TableCell>
-              <TableCell>
-                {activity.price ? <PriceTag price={activity.price} /> : "_"}
-              </TableCell>
-              <TableCell>
-                {activity.from ? (
-                  <Link href={`/wallet/${activity.from}`}>
-                    {ownerOrShortAddress({
-                      ownerAddress: activity.from,
-                      address,
-                    })}
-                  </Link>
-                ) : (
-                  "_"
-                )}
-              </TableCell>
-              <TableCell>
-                {activity.to ? (
-                  <Link href={`/wallet/${activity.to}`}>
-                    {ownerOrShortAddress({
-                      ownerAddress: activity.to,
-                      address,
-                    })}
-                  </Link>
-                ) : (
-                  "_"
-                )}
-              </TableCell>
-              <TableCell>
-                {activity.time_stamp ? timeSince(activity.time_stamp) : "_"}
-              </TableCell>
-              <TableCell className="pr-5">
-                <Button asChild size="icon" variant="outline">
-                  <ExternalLink href="/">
-                    <ArrowUpRight className="size-5" />
-                  </ExternalLink>
-                </Button>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+                </TableCell>
+                <TableCell>
+                  {activity.price ? <PriceTag price={activity.price} /> : "_"}
+                </TableCell>
+                <TableCell>
+                  {activity.from ? (
+                    <Link href={`/wallet/${activity.from}`}>
+                      {ownerOrShortAddress({
+                        ownerAddress: activity.from,
+                        address,
+                      })}
+                    </Link>
+                  ) : (
+                    "_"
+                  )}
+                </TableCell>
+                <TableCell>
+                  {activity.to ? (
+                    <Link href={`/wallet/${activity.to}`}>
+                      {ownerOrShortAddress({
+                        ownerAddress: activity.to,
+                        address,
+                      })}
+                    </Link>
+                  ) : (
+                    "_"
+                  )}
+                </TableCell>
+                <TableCell>
+                  {activity.time_stamp ? timeSince(activity.time_stamp) : "_"}
+                </TableCell>
+                <TableCell className="pr-5">
+                  <Button asChild size="icon" variant="outline">
+                    <ExternalLink href="/">
+                      <ArrowUpRight className="size-5" />
+                    </ExternalLink>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      {portfolioActivity.length === 0 && (
+        <div className="flex flex-col items-center gap-3 pt-8 text-muted-foreground">
+          <NoActivity size={42} />
+          <p className="text-xl font-semibold">No activity yet!</p>
+        </div>
+      )}
+    </>
   );
 }
--- apps/arkmarket/src/app/wallet/[walletAddress]/components/portfolio-items-data-list-view.tsx
+++ apps/arkmarket/src/app/wallet/[walletAddress]/components/portfolio-items-data-list-view.tsx
@@ -4,7 +4,7 @@ import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 import { cn, ellipsableStyles, formatUnits, timeSince } from "@ark-market/ui";
 import { Button } from "@ark-market/ui/button";
-import { Ethereum } from "@ark-market/ui/icons";
+import { Ethereum, NoResult } from "@ark-market/ui/icons";
 import {
   Table,
   TableBody,
@@ -47,144 +47,152 @@ export default function PortfolioItemsDataListView({
   });
 
   return (
-    <Table ref={tableRef}>
-      <TableHeader className="h-12">
-        <TableRow
-          className={cn(
-            "absolute grid w-full items-center",
-            gridTemplateColumnValue,
-          )}
+    <>
+      <Table ref={tableRef}>
+        <TableHeader className="h-12">
+          <TableRow
+            className={cn(
+              "absolute grid w-full items-center",
+              gridTemplateColumnValue,
+            )}
+          >
+            <TableHead className="sticky top-0 flex items-center bg-background pl-5">
+              Item
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              List price
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Best offer
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Floor
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background">
+              Received date
+            </TableHead>
+            <TableHead className="sticky top-0 flex items-center bg-background"></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          className="font-numbers relative font-medium"
+          style={{
+            height: `${rowVirtualizer.getTotalSize() + 2}px`, // Tells scrollbar how big the table is
+          }}
         >
-          <TableHead className="sticky top-0 flex items-center bg-background pl-5">
-            Item
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            List price
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Best offer
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Floor
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background">
-            Received date
-          </TableHead>
-          <TableHead className="sticky top-0 flex items-center bg-background"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody
-        className="font-numbers relative font-medium"
-        style={{
-          height: `${rowVirtualizer.getTotalSize() + 2}px`, // Tells scrollbar how big the table is
-        }}
-      >
-        {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-          const token = walletTokens[virtualRow.index];
-          if (token === undefined) {
-            return null;
-          }
-          const canListItem = isOwner && !token.list_price;
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const token = walletTokens[virtualRow.index];
+            if (token === undefined) {
+              return null;
+            }
+            const canListItem = isOwner && !token.list_price;
 
-          return (
-            <TableRow
-              className={cn(
-                "group absolute grid h-[4.6875rem] w-full items-center",
-                gridTemplateColumnValue,
-              )}
-              data-index={virtualRow.index} // Needed for dynamic row height measurement
-              key={`${token.collection_address}-${token.token_id}`}
-              ref={(node) => rowVirtualizer.measureElement(node)} // Measure dynamic row height
-              style={{
-                transform: `translateY(${virtualRow.start}px)`,
-              }}
-            >
-              <TableCell className="pl-5">
-                <div className="flex items-center gap-4">
-                  <Media
-                    alt={token.metadata?.name ?? "Empty NFT"}
-                    className="h-[2.625rem] w-[2.625rem] rounded-md object-contain"
-                    src={token.metadata?.image}
-                    mediaKey={token.metadata?.image_key}
-                    height={94}
-                    width={94}
-                  />
+            return (
+              <TableRow
+                className={cn(
+                  "group absolute grid h-[4.6875rem] w-full items-center",
+                  gridTemplateColumnValue,
+                )}
+                data-index={virtualRow.index} // Needed for dynamic row height measurement
+                key={`${token.collection_address}-${token.token_id}`}
+                ref={(node) => rowVirtualizer.measureElement(node)} // Measure dynamic row height
+                style={{
+                  transform: `translateY(${virtualRow.start}px)`,
+                }}
+              >
+                <TableCell className="pl-5">
+                  <div className="flex items-center gap-4">
+                    <Media
+                      alt={token.metadata?.name ?? "Empty NFT"}
+                      className="h-[2.625rem] w-[2.625rem] rounded-md object-contain"
+                      src={token.metadata?.image}
+                      mediaKey={token.metadata?.image_key}
+                      height={94}
+                      width={94}
+                    />
 
-                  <p className={cn("w-full", ellipsableStyles)}>
-                    {token.metadata?.name ?? token.token_id}
-                  </p>
-                </div>
-              </TableCell>
-              <TableCell>
-                {token.list_price ? (
-                  <div className="flex items-center">
-                    <Ethereum className="size-4" />
-                    <p>
-                      {formatUnits(token.list_price, 18)}{" "}
-                      <span className="text-muted-foreground">ETH</span>
+                    <p className={cn("w-full", ellipsableStyles)}>
+                      {token.metadata?.name ?? token.token_id}
                     </p>
                   </div>
-                ) : (
-                  "_"
-                )}
-              </TableCell>
-              <TableCell>
-                {token.best_offer ? (
-                  <div className="flex items-center">
-                    <Ethereum className="size-4" />
-                    <p>
-                      {formatUnits(token.best_offer, 18)}{" "}
-                      <span className="text-muted-foreground">ETH</span>
-                    </p>
-                  </div>
-                ) : (
-                  "_"
-                )}
-              </TableCell>
-              <TableCell>
-                {token.floor ? (
-                  <div className="flex items-center">
-                    <Ethereum className="size-4" />
-                    <p>
-                      {formatUnits(token.floor, 18)}{" "}
-                      <span className="text-muted-foreground">ETH</span>
-                    </p>
-                  </div>
-                ) : (
-                  "_"
-                )}
-              </TableCell>
-              <TableCell>
-                {token.received_at ? timeSince(token.received_at) : "_"}
-              </TableCell>
-              <TableCell>
-                {canListItem ? (
-                  <TokenActionsCreateListing token={token}>
+                </TableCell>
+                <TableCell>
+                  {token.list_price ? (
+                    <div className="flex items-center">
+                      <Ethereum className="size-4" />
+                      <p>
+                        {formatUnits(token.list_price, 18)}{" "}
+                        <span className="text-muted-foreground">ETH</span>
+                      </p>
+                    </div>
+                  ) : (
+                    "_"
+                  )}
+                </TableCell>
+                <TableCell>
+                  {token.best_offer ? (
+                    <div className="flex items-center">
+                      <Ethereum className="size-4" />
+                      <p>
+                        {formatUnits(token.best_offer, 18)}{" "}
+                        <span className="text-muted-foreground">ETH</span>
+                      </p>
+                    </div>
+                  ) : (
+                    "_"
+                  )}
+                </TableCell>
+                <TableCell>
+                  {token.floor ? (
+                    <div className="flex items-center">
+                      <Ethereum className="size-4" />
+                      <p>
+                        {formatUnits(token.floor, 18)}{" "}
+                        <span className="text-muted-foreground">ETH</span>
+                      </p>
+                    </div>
+                  ) : (
+                    "_"
+                  )}
+                </TableCell>
+                <TableCell>
+                  {token.received_at ? timeSince(token.received_at) : "_"}
+                </TableCell>
+                <TableCell>
+                  {canListItem ? (
+                    <TokenActionsCreateListing token={token}>
+                      <Button
+                        className="w-full opacity-0 transition-opacity focus-visible:opacity-100 group-hover:opacity-100"
+                        size="xl"
+                      >
+                        List for sale
+                      </Button>
+                    </TokenActionsCreateListing>
+                  ) : (
                     <Button
                       className="w-full opacity-0 transition-opacity focus-visible:opacity-100 group-hover:opacity-100"
                       size="xl"
+                      asChild
                     >
-                      List for sale
+                      <Link
+                        href={`/token/${token.collection_address}/${token.token_id}`}
+                      >
+                        Details
+                      </Link>
                     </Button>
-                  </TokenActionsCreateListing>
-                ) : (
-                  <Button
-                    className="w-full opacity-0 transition-opacity focus-visible:opacity-100 group-hover:opacity-100"
-                    size="xl"
-                    asChild
-                  >
-                    <Link
-                      href={`/token/${token.collection_address}/${token.token_id}`}
-                    >
-                      Details
-                    </Link>
-                  </Button>
-                )}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+                  )}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      {walletTokens.length === 0 && (
+        <div className="flex flex-col items-center gap-3 pt-8 text-muted-foreground">
+          <NoResult size={42} />
+          <p className="text-xl font-semibold">No items yet!</p>
+        </div>
+      )}
+    </>
   );
 }
