ef0998f82d53f94d0976a0056d364f25b312d785
paul launay <launay.p@gmail.com>
2024-09-12 01:06:20 +0200

    feat(token): add global buy in progress indicator

--- apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-listing-badge.tsx
+++ apps/arkmarket/src/app/token/[contractAddress]/[tokenId]/components/token-actions-listing-badge.tsx
@@ -9,18 +9,23 @@ export default function TokenActionsListingBadge({
   isListed,
   isAuction,
 }: ListingBadgeProps) {
+  const borderClass = isListed
+    ? isAuction
+      ? "border-green-500 text-green-500"
+      : "border-blue-500 text-blue-500"
+    : "border-orange-500 text-orange-500";
+
+  const statusText = isListed
+    ? isAuction
+      ? "Up for auction"
+      : "For sale"
+    : "Not for sale";
+
   return (
     <div
-      className={cn(
-        "w-fit rounded-xl border border-blue-500 px-4 py-2 text-sm text-blue-500",
-        isListed
-          ? isAuction
-            ? "border-green-500 text-green-500"
-            : "border-blue-500 text-blue-500"
-          : "border-orange-500 text-orange-500",
-      )}
+      className={cn("w-fit rounded-xl border px-4 py-2 text-sm", borderClass)}
     >
-      {isListed ? (isAuction ? "Up for auction" : "For sale") : "Not for sale"}
+      {statusText}
     </div>
   );
 }
