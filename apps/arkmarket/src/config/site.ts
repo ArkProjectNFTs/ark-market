075f0e882c478997ac2e211fa7e73320ade25229
Yohan @ ScreenshotLabs <yohantancrez@gmail.com>
2024-09-19 17:23:56 +0200

    feat: links (#155)

--- apps/arkmarket/src/config/site.ts
+++ apps/arkmarket/src/config/site.ts
@@ -1,13 +1,29 @@
-export const siteConfig = {
-  name: "ArkMarket",
-  url: "https://market.arkproject.dev",
-  ogImage: "",
-  description:
-    "Clean and simple ArkProject marketplace starter repo using Turbo repo.",
-  links: {
-    twitter: "https://twitter.com/ArkProjectNFTs",
-    github: "https://github.com/ArkProjectNFTs/ark-market",
-  },
-};
+import { env } from "~/env";
+
+export const siteConfig =
+  env.NEXT_PUBLIC_THEME === "default"
+    ? {
+        name: "ArkMarket",
+        url: "https://market.arkproject.dev",
+        ogImage: "",
+        description:
+          "Clean and simple ArkProject marketplace starter repo using Turbo repo.",
+        links: {
+          twitter: "https://twitter.com/ArkProjectNFTs",
+          discord: "https://discord.gg/4sX8ZEUPUW",
+          github: "https://github.com/ArkProjectNFTs/ark-market",
+          telegram: "https://t.me/arkprojectnfts",
+        },
+      }
+    : {
+        name: "Unframed",
+        url: "https://market.arkproject.dev",
+        ogImage: "",
+        description:
+          "Clean and simple ArkProject marketplace starter repo using Turbo repo.",
+        links: {
+          twitter: "https://x.com/unframedco",
+        },
+      };
 
 export type SiteConfig = typeof siteConfig;
