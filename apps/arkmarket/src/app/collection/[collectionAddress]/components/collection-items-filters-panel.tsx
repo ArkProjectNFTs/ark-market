e446332ed1590c82f9a294cc99d4e1f9303ef847
paul launay <launay.p@gmail.com>
2024-09-19 02:23:34 +0200

    feat(collection): add collection filter trait search

--- apps/arkmarket/src/app/collection/[collectionAddress]/components/collection-items-filters-panel.tsx
+++ apps/arkmarket/src/app/collection/[collectionAddress]/components/collection-items-filters-panel.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { SearchInput } from "@ark-market/ui/search-input";
 
 import type { Filters } from "~/types";
 import getCollectionTraits from "~/lib/getCollectionTraits";
@@ -21,6 +24,7 @@ export default function CollectionItemsFiltersPanel({
     queryKey: ["collectionTraits", collectionAddress],
     queryFn: () => getCollectionTraits({ collectionAddress }),
   });
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleTraitChange = async (name: string, value: string) => {
     const values = filters.traits[name] ?? [];
@@ -46,16 +50,48 @@ export default function CollectionItemsFiltersPanel({
     return null;
   }
 
+  const filteredData = Object.fromEntries(
+    Object.entries(data).map(([key, value]) => {
+      const filtered = Object.fromEntries(
+        Object.entries(value).filter(([k]) =>
+          k.toLowerCase().includes(searchQuery.toLowerCase()),
+        ),
+      );
+
+      return [key, filtered];
+    }),
+  );
+
+  const hasTraits = Object.values(filteredData).some(
+    (trait) => Object.keys(trait).length > 0,
+  );
+
   return (
     <div className="no-scrollbar sticky top-[var(--site-header-height)] z-10 hidden h-[calc(100vh-var(--site-header-height)-var(--site-footer-height))] w-64 flex-shrink-0 overflow-y-auto border-r border-border lg:block">
       <div className="">
-        <div className="px-5 py-5 text-base font-bold">Traits</div>
+        <div className="px-5 py-4 text-base font-bold">Traits</div>
+        <div className="px-4 pb-4">
+          <SearchInput
+            value={searchQuery}
+            className="h-10 focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+            placeholder="Search"
+            type="text"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
         <div className="flex flex-col gap-2">
-          {Object.keys(data).map((key: string) => (
+          {searchQuery.length > 0 && !hasTraits && (
+            <div className="px-5">
+              <div className="mb-2 text-sm text-muted-foreground">
+                No traits found for "{searchQuery}"
+              </div>
+            </div>
+          )}
+          {Object.keys(filteredData).map((key: string) => (
             <CollectionItemsFiltersTrait
               key={key}
               name={key}
-              trait={data[key] ?? {}}
+              trait={filteredData[key] ?? {}}
               selectedTraits={filters.traits}
               onChange={handleTraitChange}
             />
