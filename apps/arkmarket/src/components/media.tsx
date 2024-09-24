0bc08a1d18033d98a87a9a8f0fbe8b1e3a968f38
YohanTz <yohantancrez@gmail.com>
2024-09-24 17:01:12 +0200

    feat: image placeholders

--- apps/arkmarket/src/components/media.tsx
+++ apps/arkmarket/src/components/media.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import type { PropsWithClassName } from "@ark-market/ui";
 import { cn } from "@ark-market/ui";
+import { NoImage } from "@ark-market/ui/icons";
 import { Skeleton } from "@ark-market/ui/skeleton";
 
 import { env } from "~/env";
@@ -45,35 +46,7 @@ function MediaPlaceholder({ className }: PropsWithClassName) {
         className,
       )}
     >
-      <svg
-        width="79"
-        height="79"
-        viewBox="0 0 79 79"
-        fill="none"
-        className="text-background"
-      >
-        <path
-          d="M62.3462 10.2844H16.7915C13.1973 10.2844 10.2837 13.1981 10.2837 16.7922V62.3469C10.2837 65.9411 13.1973 68.8547 16.7915 68.8547H62.3462C65.9403 68.8547 68.854 65.9411 68.854 62.3469V16.7922C68.854 13.1981 65.9403 10.2844 62.3462 10.2844Z"
-          stroke="currentColor"
-          strokeWidth="3.90469"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M29.8076 36.3159C33.4018 36.3159 36.3154 33.4023 36.3154 29.8081C36.3154 26.2139 33.4018 23.3003 29.8076 23.3003C26.2134 23.3003 23.2998 26.2139 23.2998 29.8081C23.2998 33.4023 26.2134 36.3159 29.8076 36.3159Z"
-          stroke="currentColor"
-          strokeWidth="3.90469"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M68.854 49.3312L58.8124 39.2897C57.592 38.0696 55.9371 37.3843 54.2114 37.3843C52.4858 37.3843 50.8308 38.0696 49.6104 39.2897L20.0454 68.8547"
-          stroke="currentColor"
-          strokeWidth="3.90469"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <NoImage />
     </div>
   );
 }
