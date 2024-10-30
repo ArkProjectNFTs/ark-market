eba5838a4bc775ef72e023958b86fae00faddbf3
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-10-30 12:38:19 +0100

    chore: remove usage of `NEXT_PUBLIC_ORDERBOOK_API_URL` environment variable (#200)
    This PR removes the dependency on the `NEXT_PUBLIC_ORDERBOOK_API_URL`
    environment variable within the application. All references to this
    variable have been updated.

--- apps/arkmarket/src/lib/getSystemStatus.ts
+++ apps/arkmarket/src/lib/getSystemStatus.ts
@@ -2,7 +2,7 @@ import type { SystemStatus } from "~/types";
 import { env } from "~/env";
 
 export default async function getSystemStatus() {
-  const response = await fetch(env.NEXT_PUBLIC_ORDERBOOK_API_URL);
+  const response = await fetch(env.NEXT_PUBLIC_MARKETPLACE_API_URL);
   const data = (await response.json()) as SystemStatus;
 
   return data;
