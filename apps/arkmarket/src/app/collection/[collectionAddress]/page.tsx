32e052e4d9d72f8616bee812eb20b530dec5feb8
paul launay <launay.p@gmail.com>
2024-11-20 18:11:04 +0100

    feat(market): add metadata

--- apps/arkmarket/src/app/collection/[collectionAddress]/page.tsx
+++ apps/arkmarket/src/app/collection/[collectionAddress]/page.tsx
@@ -1,10 +1,35 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
+import { env } from "~/env";
 import getCollection from "~/lib/getCollection";
 import CollectionBanner from "./components/collection-banner";
 import CollectionHeader from "./components/collection-header";
 import CollectionItems from "./components/collection-items";
 import MobileCollectionHeader from "./components/mobile-collection-header";
+
+interface GenerateMetadataProps {
+  params: Promise<{ collectionAddress: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: GenerateMetadataProps): Promise<Metadata> {
+  const collectionAddress = (await params).collectionAddress;
+  const collection = await getCollection({ collectionAddress });
+  const platform =
+    env.NEXT_PUBLIC_THEME === "unframed" ? "Unframed" : "Ark Market";
+  const name = collection?.name ?? "Collection";
+
+  return {
+    title: `${name} | ${platform}`,
+    openGraph: {
+      images: [
+        `https://ark-market-unframed.vercel.app/api/og/collection?collection_address=${collectionAddress}`,
+      ],
+    },
+  };
+}
 
 interface CollectionPageProps {
   params: {
@@ -21,32 +46,23 @@ export default async function CollectionPage({ params }: CollectionPageProps) {
   }
 
   return (
-    <>
-      <head>
-        <meta
-          property="og:image"
-          content={`https://ark-market-unframed.vercel.app/api/og/collection?collection_address=${collectionAddress}`}
-        />
-      </head>
-
-      <div className="flex min-h-[calc(100vh-var(--site-header-height))] flex-col">
-        <CollectionBanner
-          className="hidden md:block"
-          collectionAddress={collectionAddress}
-        />
-        <CollectionHeader
-          collectionAddress={collectionAddress}
-          collection={collection}
-        />
-        <MobileCollectionHeader
-          collectionAddress={collectionAddress}
-          collection={collection}
-        />
-        <CollectionItems
-          collectionAddress={collectionAddress}
-          collectionTokenCount={collection.token_count}
-        />
-      </div>
-    </>
+    <div className="flex min-h-[calc(100vh-var(--site-header-height))] flex-col">
+      <CollectionBanner
+        className="hidden md:block"
+        collectionAddress={collectionAddress}
+      />
+      <CollectionHeader
+        collectionAddress={collectionAddress}
+        collection={collection}
+      />
+      <MobileCollectionHeader
+        collectionAddress={collectionAddress}
+        collection={collection}
+      />
+      <CollectionItems
+        collectionAddress={collectionAddress}
+        collectionTokenCount={collection.token_count}
+      />
+    </div>
   );
 }
